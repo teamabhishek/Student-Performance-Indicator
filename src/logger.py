3eeaa8fb26241697a2d07061cb1820790a5f3068
Abhishek Pathak <abhishekkumarpathak2002@gmail.com>
2023-05-14 13:35:17 +0530

    Project part

--- src/logger.py
+++ src/logger.py
@@ -1,20 +1,15 @@
-import sys
-from src.logger import logging
+import logging
+import os
+from datetime import datetime
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-    return error_message
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-    
-
-class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
-    def __str__(self):
-        return self.error_message
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
